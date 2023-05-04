1598f391a1e666ac9051d822438049bce3cbf263
TineCrnugelj <tine.crnugelj@gmail.com>
2023-05-04 12:31:49 +0200

    jewlery page implementation

--- pa2/implementation-extraction/regex.py
+++ pa2/implementation-extraction/regex.py
@@ -29,3 +29,40 @@ def page_rtv(html):
         "Content": re.findall(content, html)[0]
     }
     return json
+
+def page_overstock(html):
+    title_re = r'</table></td><td valign="top"> \n<a href="[^\"]*"><b>(.*?)</b>'
+    title = re.compile(title_re)
+
+    content_re = r'<span class="normal">([^<]*)<'
+    content = re.compile(content_re)
+
+    list_price_re = r'<s>([^<]*)</s>'
+    list_price = re.compile(list_price_re)
+
+    price_re = r'<span class="bigred"><b>([^<]*)</b></span>'
+    price = re.compile(price_re)
+
+    saving_re = r'<span class="littleorange">([^\s]*)\s[^<]*</span>'
+    saving = re.compile(saving_re)
+
+    saving_percent_re = r'<span class="littleorange">[^\(]*\(([^<]*)\)</span>'
+    saving_percent = re.compile(saving_percent_re)
+
+    json = {
+        "Title": re.findall(title, html)[0],
+        "Content": re.findall(content, html)[0],
+        "ListPrice": re.findall(list_price, html)[0],
+        "Price": re.findall(price, html)[0],
+        "Saving": re.findall(saving, html)[0],
+        "SavingPercent": re.findall(saving_percent, html)[0]
+    }
+    return json
+
+def page_insert_name(html):
+    # TODO
+    json = {
+
+    }
+
+    return json
--- pa2/implementation-extraction/run-extraction.py
+++ pa2/implementation-extraction/run-extraction.py
@@ -4,12 +4,18 @@ import regex
 html_rtv_audi = open('../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html').read()
 html_rtv_volvo = open('../input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html').read()
 
+html_jewlery_a = open("../input-extraction/overstock.com/jewelry01.html", encoding='cp1252').read()
+html_jewlery_b = open("../input-extraction/overstock.com/jewelry02.html", encoding='cp1252').read()
+
 ALGORITHM = sys.argv[1]
 
 if ALGORITHM == "A":
     print('Running algorithm using regular expressions')
     print(regex.page_rtv(html_rtv_audi))
     print(regex.page_rtv(html_rtv_volvo))
+    print(regex.page_overstock(html_jewlery_a))
+    print(regex.page_overstock(html_jewlery_b))
+    # TODO custom page
 
 elif ALGORITHM == "B":
     print('Running algorithm using XPath')
